61a162bc829b5ba0def39dac62ad0fb80b50c287
Abderrazak <abderrazak.derwich@gmail.com>
2025-03-25 11:04:39 +0100

    fix table name

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -3,10 +3,10 @@ CREATE TABLE IF NOT EXISTS products (
     product VARCHAR(50) NOT NULL,
     code VARCHAR(50) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
-    UNIQUE KEY (name)
+    UNIQUE KEY (product)
 );
 
-INSERT IGNORE INTO product (product, code, price) VALUES
+INSERT IGNORE INTO products (product, code, price) VALUES
 ('Red Widget', 'R01', 32.95),
 ('Green Widget', 'G01', 24.95),
 ('Blue Widget', 'B01', 7.95);
